83dcc5e0dbc3b32dee0b4c24161280a0f71e6aef
haroharsh <harshitnariyal@gmail.com>
2026-02-03 20:50:54 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MediaNest</title>
+    <link rel="stylesheet" href="./assets/base.css">
+</head>
+<div>
+    <h1>Welcome to MediaNest</h1>
+    <p>Your one-stop solution for media management.</p>
+</div>
+</html>
